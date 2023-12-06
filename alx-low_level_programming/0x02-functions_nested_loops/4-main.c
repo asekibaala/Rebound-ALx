a841e568adbe9f3d6e974b0a6aa0de645f153308
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2023-12-06 07:30:53 +0300

    0x02-functions_nested_loops day 3

---
+++ alx-low_level_programming/0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+    int r;
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha(1008);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
---
+++ alx-low_level_programming/0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10();
+int _islower(int c);
+int _isalpha(int c);
+
+#endif
