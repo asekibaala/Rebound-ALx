c2dccc06e742603cab8d965b71a2a6fe661d0f24
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2023-12-08 07:23:56 +0300

    rebound day 4

---
+++ alx-low_level_programming/0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int r;
+
+int main()
+{
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
