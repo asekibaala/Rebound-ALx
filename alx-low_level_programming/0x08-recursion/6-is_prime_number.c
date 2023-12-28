f5efc730c2f7fe453e7a94c859aa5d064dfdf7eb
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2023-12-28 10:22:52 +0300

    rebound alx module 8

---
+++ alx-low_level_programming/0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
