c2d27b85c4c8104a5d2a10985c740e116824fd65
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-03-16 18:11:21 +0300

    rebound-singly linked lists

---
+++ alx-low_level_programming/0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+size_t list_len(const list_t *h)
+{
+    size_t nelem;
+    nelem =0;
+    while (h != NULL)
+    {
+        h = h->next;
+        nelem++;
+    }
+    return (nelem);
+}
