acdd7564d64839dd6a90ac991980a85487569478
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-04-02 06:12:17 +0300

    add new node at start of dobuly linked list

--- alx-low_level_programming/0x17-doubly_linked_lists/lists.h
+++ alx-low_level_programming/0x17-doubly_linked_lists/lists.h
@@ -69,4 +69,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 #endif
