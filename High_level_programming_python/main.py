2f5df0058bb56fd00f4ad01db499ba744061cdc4
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-08-26 05:31:13 +0300

    python-test_driven_development integers addition

--- High_level_programming_python/main.py
+++ High_level_programming_python/main.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 print("Best School")
+
+s = "hello"
+print(s)  # Output: hello
+print(id(s))  # Print the memory address of the original string
+
+# Trying to change the first character of the string
+s = "j" + s[1:]
+print(s)  # Output: jello
+print(id(s))  # Print the memory address of the new string
