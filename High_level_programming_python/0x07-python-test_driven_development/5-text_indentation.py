03821e74aa5c1ad2149b13008250f0d891491702
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-08-30 11:24:14 +0300

    python-test_driven_development

---
+++ High_level_programming_python/0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
