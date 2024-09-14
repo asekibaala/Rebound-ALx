64a9082fc99eabbe50348a34b8dae3d311c9b9d0
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-09-14 04:48:28 +0300

    python-input_output

--- High_level_programming_python/0x0B-python-input_output/4-main.py
+++ High_level_programming_python/0x0B-python-input_output/4-main.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-from_json_string = __import__('4-from_json_string').from_json_string
-
-s_my_list = "[1, 2, 3]"
-my_list = from_json_string(s_my_list)
-print(my_list)
-print(type(my_list))
-
-s_my_dict = """
-{"is_active": true, "info": {"age": 36, "average": 3.14}, 
-"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+""" My class module
 """
-my_dict = from_json_string(s_my_dict)
-print(my_dict)
-print(type(my_dict))
 
-try:
-    s_my_dict = """
-    {"is_active": true, 12 }
+class MyClass:
+    """ My class
     """
-    my_dict = from_json_string(s_my_dict)
-    print(my_dict)
-    print(type(my_dict))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
---
+++ High_level_programming_python/0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,5 @@
+def class_to_json(obj):
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
