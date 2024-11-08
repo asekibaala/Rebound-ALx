eaac8d267ed1d2e36f7e916acab9e329fd142403
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-11-08 09:35:46 +0300

    created a Many To Many relationship between these two classes

--- sqlalchemy-tutorial/movie.py
+++ sqlalchemy-tutorial/movie.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer, Date,Table,ForeignKey
+from sqlalchemy.orm import relationship
+
 
 from base import Base
+
+movies_actors_association = Table(
+    'movies_actors', Base.metadata,
+    Column('movie_id', Integer, ForeignKey('movies.id')),
+    Column('actor_id', Integer, ForeignKey('actors.id'))
+)
 
 
 class Movie(Base):
@@ -9,6 +17,7 @@ class Movie(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     release_date = Column(Date)
+    actors = relationship("Actor", secondary=movies_actors_association)
 
     def __init__(self, title, release_date):
         self.title = title
