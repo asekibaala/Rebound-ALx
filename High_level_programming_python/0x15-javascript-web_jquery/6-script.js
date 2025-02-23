7a9168890ce7dd5e5bd148ccc4627e3077fff655
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-02-23 06:12:51 +0300

    use the JQuery API to set new header

---
+++ High_level_programming_python/0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+const $ = window.$;
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+}
+);  // When the user clicks on DIV#update_header, 
+// the text of the header must be updated to New Header!!!   
