15c2dd9a7ddae2f20f4481ec530c259048c8238c
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-03-23 08:24:34 +0300

    Update test_base_model.py

--- AIRBNBCLONE/test_base_model.py
+++ AIRBNBCLONE/test_base_model.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+# Create a new instance of BaseModel
 my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
+print(my_model.to_dict())
+
+# Save the instance
 my_model.save()
+
+# Reload objects from storage
+from models import storage
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id, obj in all_objs.items():
+    print(obj)#!/usr/bin/python3
+from models.base_model import BaseModel
+
+# Create a new instance of BaseModel
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
 print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+print(my_model.to_dict())
+
+# Save the instance
+my_model.save()
+
+# Reload objects from storage
+from models import storage
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id, obj in all_objs.items():
+    print(obj)
